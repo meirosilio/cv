96ae99b97a3a0882e4901d51d3583a66071bad6c
meirosilio <meirosilio@gmail.com>
2017-11-16 15:07:28 +0200

    add vlidation and add upcase! before save

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   has_many :categorizations
+
+  validates :name , presence: true
+  before_save upcase
+
+  private
+  def upcase
+    self.name.upcase!
+  end
+  
 end
